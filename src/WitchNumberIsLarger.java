0ed7efb252e07372eb38d24164c3d0e7afd33169
Michał Murawski <homasha@gmail.com>
2022-08-08 15:52:40 +0200

    Initial commit

---
+++ src/WitchNumberIsLarger.java
@@ -0,0 +1,10 @@
+public class WitchNumberIsLarger {
+    public static void main(String[] args) {
+        int i = 1, j = 2, k = 3;
+        LargerNumber(i, j, k);
+    }
+
+    private static void LargerNumber(int i, int j, int k) {
+        System.out.println("The largest number is: ");
+    }
+}
