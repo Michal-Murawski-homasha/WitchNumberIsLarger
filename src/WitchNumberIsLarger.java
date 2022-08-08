8fc654327e3f3260d5c7bab091ee092f778d50c2
Michał Murawski <homasha@gmail.com>
2022-08-08 16:17:39 +0200

    Count highest number

--- src/WitchNumberIsLarger.java
+++ src/WitchNumberIsLarger.java
@@ -1,10 +1,17 @@
 public class WitchNumberIsLarger {
     public static void main(String[] args) {
-        int i = 1, j = 2, k = 3;
-        LargerNumber(i, j, k);
-    }
-
-    private static void LargerNumber(int i, int j, int k) {
-        System.out.println("The largest number is: ");
+        int i = 1, j = 4, k = 3;
+        if (i < j || i < k) {
+            if (j < k) {
+                int n = k;
+                System.out.println(k + " is larger then " + i + " and " + j);
+            } else {
+                int n = j;
+                System.out.println(j + " is larger then " + i + " and " + k);
+            }
+        } else {
+            int n = i;
+            System.out.println(i + " is larger then " + j + " and " + k);
+        }
     }
 }
